3ca6d24a7e54658b9df98533fe615ed4c8783723
DhuhaAhamd <dhuha9712@gmail.com>
2022-12-29 02:16:13 +0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import SignUp from "./components/Signup";
+// import { useState } from "react";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import LogIn from "./components/LogIn";
 import "./App.css";
 import "./style.scss"
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
+      {/* <LogIn/> */}
+      <Home/>
     
     </div>
   );
