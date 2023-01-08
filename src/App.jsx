00adc98e47e95a1e93096079d9e2d12e93867b90
DhuhaAhamd <dhuha9712@gmail.com>
2023-01-08 07:42:58 +0300

    used auth context + protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-// import { useState } from "react";
+import { useContext } from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
 } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import LogIn from "./components/LogIn";
+import { AuthContext } from "./context/AuthContext";
 import "./App.css";
 import "./style.scss"
+
 function App() {
+  const {currentUser} = useContext(AuthContext)
+
+  // Protevted component
+  const ProtevtedRoute = ({children})=>{
+    if(!currentUser){
+      return <Navigate to={"/login"}/>
+    }
+  }
   return (
     <>   
     <BrowserRouter>
 <Routes>
   <Route path="/">
-    <Route index element={<Home/>}/>
+    <Route index element={
+    <ProtevtedRoute>
+      <Home/>
+      </ProtevtedRoute>
+    }/>
     <Route exact path="signup" element={<SignUp/>}/>
     <Route exact path="login" element={<LogIn/>}/>
 
   </Route>
 </Routes>
     </BrowserRouter>
-    {/* <div className="App"> */}
-      {/* <SignUp /> */}
-      {/* <LogIn/> */}
-      {/* <Home/> */}
-    
-    {/* </div> */}
+  
     </>
  
   );
