b6b7091c1188749da337ba2f0a8abf0072f7d379
DhuhaAhamd <dhuha9712@gmail.com>
2023-01-10 04:13:46 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
     if(!currentUser){
       return <Navigate to={"/login"}/>
     }
+    return children
   }
   return (
     <>   
--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,15 +1,40 @@
-import { Link } from "react-router-dom";
+import {signInWithEmailAndPassword} from "firebase/auth"
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import {useState} from "react"
+
 
 function LogIn() {
+
+    const [err, setErr] = useState(false);
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+ 
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+        navigate("/")
+        
+    
+    } catch (error) {
+      setErr(true);
+    }
+  };
     return (  <>
        <div className="formContainer">
         <div className="formWrapper">
             <span className="logo">Chat App</span>
             <span className="title">Log In</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="email" placeholder="Email"/>
                 <input type="password" placeholder="Password"/>
                 <button>Log In</button>
+                {err && <span>Somthing went wrong</span>}
+
             </form>
             <p>You don't have an account? <Link to={"/signup"}>Sign up</Link> </p>
         </div>
