de87c5cfed9962878df05dd039735426365a824a
DhuhaAhamd <dhuha9712@gmail.com>
2023-03-05 05:33:08 +0300

    created chat context

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthContextProvider } from './context/AuthContext'
+import { ChatContextProvider } from './context/ChatContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
+    <ChatContextProvider>
   <React.StrictMode>
     <App /> 
   </React.StrictMode>
+  </ChatContextProvider>
   </AuthContextProvider>,
 )
