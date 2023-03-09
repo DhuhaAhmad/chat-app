73f25265046532c5ad626be1f1edfbeb3470fa14
DhuhaAhamd <dhuha9712@gmail.com>
2023-03-09 06:01:12 +0300

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,6 @@ function Navbar() {
     return ( <>
     <div className="navbar">
         <span className="logo">Chat App</span>
-        {/* <span className="logo">{import.meta.env.VITE_API_KEY}</span> */}
         <div className="user">
         <img src={currentUser.photoURL}/>
             <span>{currentUser.displayName}</span>
